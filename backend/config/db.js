ccdf60fe8fb3e850d3e632f69aabdffe333fa0a2
ChaitanyaYK <chaitanyakolhe354@gmail.com>
2025-03-25 20:13:37 +0530

    initial commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,5 +2,5 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     
-    await mongoose.connect("mongodb+srv://chaitanya:foodapp@cluster1.ddrxq.mongodb.net/").then(()=>console.log("DB Connected"))
+    await mongoose.connect(process.env.DATABASE_KEY).then(()=>console.log("DB Connected"))
 }
